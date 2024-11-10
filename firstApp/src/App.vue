7fdd5f7460b8ffd9dcd03bc68e2c56ae1f2cd734
ZeynepMirzooglu <zeynepmirzooglu@gmail.com>
2024-11-10 16:57:37 +0300

    Life cycle hookslar

--- firstApp/src/App.vue
+++ firstApp/src/App.vue
@@ -3,78 +3,19 @@
 <div>
   <app-header/>
   <div class="container">
-    <!-- <Students /> -->
-    <!-- <Personel :user ="person.name"   :lastName ="person.lastName" :userAge="person.age" @updateLastname="updateLastName"/>
-    <button @click="updateName">Bilgileri Güncelle</button> -->
-  <!-- <StudentsScore>
-    <template v-slot:name>
-      <ul>
-        <li v-for="student in students" :key="student.id">
-          <span>{{student.name}} :</span> <span>{{student.score}}</span>
-        </li>
-    </ul>
-    </template>
-    <template v-slot:example>
-        <h2>Example</h2>
-    </template>
-    <template v-slot>
-      <h3>V-slot kullanımı</h3>
-  </template>
-  </StudentsScore> -->
-  <button @click="activeComp = StudentOne">Öğrenci 1 Page</button>
-  <button @click="activeComp = StudentTwo">Öğrenci 2 Page</button>
-  <!-- <StudentOne v-if="activeComp === StudentOne"/>
-  <StudentTwo v-if="activeComp === StudentTwo"/> -->
-  <component :is="activeComp"></component>
+<Lifecycle v-if="showComponent"/>
   </div>
   <Footer/>
 </div>
 </template>
 <script setup>
-import { ref,reactive ,provide, shallowRef} from 'vue';
+import { ref} from 'vue';
 import Footer from '@/components/Fixed/Footer.vue';
-import StudentOne from './components/StudentsMath/StudentOne.vue';
-import StudentTwo from './components/StudentsMath/StudentTwo.vue';
-//import Personel from './components/UserInformation/Personel.vue';
-//import StudentsScore from './components/Students/StudentsScore.vue';
-//const name= ref('Aslan Kaplan');
+import Lifecycle from './components/Cycle/Lifecycle.vue';
 
-// const person = reactive({
-//   name:'Zeynep',
-//   lastName:'Mirzooğlu',
-//   age:34,
+const showComponent = ref(true);
 
-// })
-// const students = reactive([{
-//   id:1,
-//   name:'Zeynep',
-//   lastName:'Mirzooğlu',
-//   score:85,
-// },
-// {
-//   id:2,
-//   name:'Gani',
-//   lastName:'Mirzooğlu',
-//   score:50,
-// },
-// {
-//   id:3,
-//   name:'Muhammed',
-//   lastName:'Mirzooğlu',
-//   score:95,
-// }])
-// const updateName=()=>{
-//     person.name ='Aslan'
-//  }
-//  const updateLastName=(lastName)=>{
-// person.lastName = lastName;
-//  }
-//  const updateScore=()=>{
-//   students[0].score= 100;
-// }
-const activeComp= shallowRef(StudentOne);
- //provide('students',{students,updateScore})
-
+setTimeout(()=>{showComponent.value = false;},4000);
 </script>
 
 <style>
---
+++ firstApp/src/components/Cycle/Lifecycle.vue
@@ -0,0 +1,31 @@
+<template>
+   <div>
+    <div>    {{name}}</div>
+
+    <button @click="name = 'Aslan'">Değiştir</button>
+
+</div>
+  
+</template>
+<script setup>
+import { onMounted,onBeforeMount,ref,onBeforeUpdate,onUpdated,onBeforeUnmount,onUnmounted } from 'vue';
+const name= ref("Zeynep");
+onBeforeMount(() => {
+  console.log("OnBeforeMount")
+});
+onMounted(() => {
+  console.log("onMountend")
+});
+onBeforeUpdate(() => {
+    console.log("onBeforeUpdate")
+});
+onUpdated(() => {
+  console.log("onupdated");
+});
+onBeforeUnmount(() => {
+    console.log("onBeforeUnMount");
+});
+onUnmounted(() => {
+    console.log("onUnMount");
+});
+</script>
